e6dfda2b1d9c78cf8b333319d71dc74368d4f090
Ashish Kumar Singh <ashishsingh.ashishya@gmail.com>
2019-02-18 15:46:33 +0530

    create custom event according to the user

--- src/js/components/calender/demos/editCalendar.js
+++ src/js/components/calender/demos/editCalendar.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import moment from 'moment';
 import BigCalendar from 'react-big-calendar';
-import DatePicker from "react-datepicker";
 import events from '../../../containers/calender/BigCalender/event';
 import { format } from 'date-fns';
 import { BaseDialog } from '../../Base';
 import { TextField } from '../../TextField';
 import { BaseButton } from '../../Buttons/Base';
 import styles from '../../../../style/main.scss';
-
-require("react-datepicker/dist/react-datepicker.css");
 
 const propTypes = {}
 
@@ -19,16 +15,25 @@ class Selectable extends React.Component {
 
         this.state = {
             events: events,
+            showModel: false,
             handleModel: false,
             title: '',
             start: '',
             end: '',
+            viewEvent: '',
+            startTime: '',
+            endTime: '',
         }
+    }
+
+    formatTimeDisplay = (date, defaultText) => {
+        if (!date) return defaultText;
+        return format(date, 'hh:mm a');
     }
 
     formatDateDisplay = (date, defaultText) => {
         if (!date) return defaultText;
-        return format(date, 'hh:mm a');
+        return format(date, 'DD/MM/YYYY');
     }
 
     handleClose = () => {
@@ -41,7 +46,21 @@ class Selectable extends React.Component {
         this.setState({
             handleModel: true,
             start: start,
-            end: end
+            end: end,
+            startTime: this.formatTimeDisplay(start),
+            endTime: this.formatTimeDisplay(end),
+        })
+    }
+
+    addEvent = () => {
+        const start = new Date();
+        const end = new Date();
+        this.setState({
+            handleModel: true,
+            start: start,
+            end: end,
+            startTime: this.formatTimeDisplay(start),
+            endTime: this.formatTimeDisplay(end),
         })
     }
 
@@ -49,17 +68,47 @@ class Selectable extends React.Component {
         let inputValue = e.target.value;
         this.setState({
             [`${key}`]: inputValue,
-        }, () => {
-            console.log('State', this.state);
         });
-
     };
 
+    handleInputTimeChange = key => e => {
+        this.setState({
+            [`${key}Time`]: e.target.value,
+        });
+    }
+
+    dateTimeBindingFormation = (date, time) => {
+        let newDate = date;
+        const YYYY = newDate.getFullYear();
+        const DD = newDate.getDate();
+        const MM = newDate.getMonth();
+        let Time = time;
+        let hh, mm;
+        if (Time.search("am") !== -1) {
+            time = time.split(':');
+            if (Time === "12:00 am") {
+                hh = '00';
+            } else {
+                hh = time[0];
+            }
+            mm = time[1].substring(0, 2);
+        } else {
+            time = time.split(':');
+            if (Time === "12:00 pm") {
+                hh = '12';
+            } else {
+                hh = parseInt(time[0]) + 12;
+            }
+            mm = time[1].substring(0, 2);
+        }
+        newDate = new Date(YYYY, MM, DD, hh, mm, 0, 0);
+        return newDate;
+    }
+
     createEvent = () => {
-        // const title = window.prompt('New Event name')
         const title = this.state.title;
-        const start = this.state.start;
-        const end = this.state.end;
+        const start = this.dateTimeBindingFormation(this.state.start, this.state.startTime);
+        const end = this.dateTimeBindingFormation(this.state.end, this.state.endTime);
         if (title)
             this.setState({
                 events: [
@@ -80,25 +129,41 @@ class Selectable extends React.Component {
             })
     }
 
-    showEvent = (title) => {
-        <BaseDialog open={true}>
-            <div>{title}</div>
-        </BaseDialog>
+    showEvent = (event) => {
+        this.setState({
+            viewEvent: event,
+            showModel: true,
+        })
+    }
+
+    handleShowModelClose = () => {
+        this.setState({
+            showModel: false
+        })
     }
 
     render() {
         const { localizer } = this.props
         return (
             <div>
+                <div className={styles.addBtn}>
+                    <BaseButton
+                        height="thin"
+                        size="tiny"
+                        color="primary"
+                        onClick={this.addEvent}
+                    > Add
+                    </BaseButton>
+                </div>
                 <BigCalendar
                     selectable
                     localizer={localizer}
                     events={this.state.events}
                     defaultView={BigCalendar.Views.MONTH}
                     scrollToTime={new Date(1970, 1, 1, 6)}
-                    onSelectEvent={event => console.log(event)}
+                    onSelectEvent={event => this.showEvent(event)}
                     onSelectSlot={this.handleOpen}
-                    style={{ height: 'calc(100vh - 140px)' }}
+                    style={{ height: 'calc(100vh - 165px)' }}
                 />
                 <BaseDialog open={this.state.handleModel} onClose={this.handleClose}>
                     <div className={styles.formEvent}>
@@ -115,20 +180,18 @@ class Selectable extends React.Component {
                         <div className={styles.eventTimeSlot}>
                             <div className={styles.eventStartTime}>
                                 <TextField
-                                    disabled
                                     type="text"
-                                    value={this.formatDateDisplay(this.state.start)}
+                                    value={this.state.startTime}
                                     placeholder={'Start Event Time'}
-                                    onChange={this.handleInputChange('start')}
+                                    onChange={this.handleInputTimeChange('start')}
                                 />
                             </div>
                             <div className={styles.eventEndTime}>
                                 <TextField
-                                    disabled
                                     type="text"
-                                    value={this.formatDateDisplay(this.state.end)}
+                                    value={this.state.endTime}
                                     placeholder={'End Event Time'}
-                                    onChange={this.handleInputChange('end')}
+                                    onChange={this.handleInputTimeChange('end')}
                                 />
                             </div>
                         </div>
@@ -140,6 +203,35 @@ class Selectable extends React.Component {
                                 onClick={this.createEvent}
                             > Submit
                             </BaseButton>
+                        </div>
+                    </div>
+                </BaseDialog>
+                <BaseDialog open={this.state.showModel} onClose={this.handleShowModelClose}>
+                    <div style={{ textAlign: 'justify', minHeight: 70 }}>
+                        <h3>Event-Name :- {this.state.viewEvent.title}</h3>
+                        <div className={styles.showEventStartTime}>
+                            <TextField
+                                disabled
+                                value={this.formatDateDisplay(this.state.viewEvent.start)}
+                            />
+                        </div>
+                        <div className={styles.showEventEndTime}>
+                            <TextField
+                                disabled
+                                value={this.formatDateDisplay(this.state.viewEvent.end)}
+                            />
+                        </div>
+                        <div className={styles.showEventStartTime}>
+                            <TextField
+                                disabled
+                                value={this.formatTimeDisplay(this.state.viewEvent.start)}
+                            />
+                        </div>
+                        <div className={styles.showEventEndTime}>
+                            <TextField
+                                disabled
+                                value={this.formatTimeDisplay(this.state.viewEvent.end)}
+                            />
                         </div>
                     </div>
                 </BaseDialog>
